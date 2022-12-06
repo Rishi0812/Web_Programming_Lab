910789a76cbe60880abab6ba5426622dede74de1
Rishi0812 <rishiramesh08@gmail.com>
2022-12-07 00:20:03 +0530

    Added 10-12

---
+++ Lab-10/index.js
@@ -0,0 +1,74 @@
+import "./App.css";
+import React, { Component } from "react";
+//import axios from "axios";
+const API_PATH = "http://localhost:3000/TEST1-PROJECT/api/index.php";
+class Validations extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
+  }
+  FormSubmittionHandler(event) {
+    event.preventDefault();
+    axios({
+      method: "post",
+      url: `${PATH}`,
+      headers: { "content-type": "application/json" },
+      data: this.state,
+    })
+      .then((result) => {
+        this.setState({
+          sent: result.data.sent,
+        });
+      })
+      .catch((error) => this.setState({ error: error.message }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Contact Me</h1>
+        <div className="container">
+          <form action="#">
+            <label>Full Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Enter your name.."
+              value={this.state.name}
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+            <br />
+            <label>Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+            <br />
+            <label>Subject</label>
+            <textarea
+              id="subject"
+              name="subject"
+              placeholder="Enter your message.."
+              value={this.state.message}
+              onChange={(e) => this.setState({ message: e.target.value })}
+            ></textarea>
+            <input
+              type="submit"
+              value="Submit"
+              onClick={(e) => this.FormSubmittionHandler(e)}
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+export default Validations;
---
+++ Lab-12/ac.js
@@ -0,0 +1,8 @@
+$(Document).ready(function () {
+  $("#hide").click(function () {
+    $("p").hide();
+  });
+  $("#show").click(function () {
+    $("p").show();
+  });
+});
